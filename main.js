9972891f6df0e62ca0a91374f32cfabbc9f59637
justinflangan123 <justin.flanagan@student.swic.edu>
2024-05-11 19:45:48 -0500

    'Select' all of the input fields

--- main.js
+++ main.js
@@ -6,3 +6,9 @@
  *  upon losing focus (`blur` event) on the input fields, we will
  * show the error. For now, we are not validating the input fields.
  */
+
+const formEls = [
+  ...Array.from(document.querySelectorAll("input")),
+  document.querySelector("#message"),
+];
+console.log(formEls);
