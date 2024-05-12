efaa0ada49007c34bcd3b5cce99463744c9ab1f1
justinflangan123 <justin.flanagan@student.swic.edu>
2024-05-11 20:07:05 -0500

    add 'Select' the submit button and event listeners to each input field

--- main.js
+++ main.js
@@ -11,4 +11,11 @@ const formEls = [
   ...Array.from(document.querySelectorAll("input")),
   document.querySelector("#message"),
 ];
-console.log(formEls);
+
+const submitBtn = document.querySelector("button[type='submit']");
+
+formEls.forEach((el) => {
+  el.addEventListener("blur", (e) => {
+    console.log("blur event", e.target.id, e.target.value);
+  });
+});
