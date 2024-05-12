6d616d74399b74d6da972fccb5ee5282b677b0bf
justinflangan123 <justin.flanagan@student.swic.edu>
2024-05-11 19:36:28 -0500

    Add todo list

---
+++ main.js
@@ -0,0 +1,8 @@
+/**
+ * TODO:
+ * 1. 'Select' all of the input fields.
+ * 2. 'Select' the submit button.
+ * 3. Add event listeners to each input field, such that
+ *  upon losing focus (`blur` event) on the input fields, we will
+ * show the error. For now, we are not validating the input fields.
+ */
